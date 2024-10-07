c9299944cc5374392fc8ad874dbd7bec7cf05eae
pc <pc@LAPTOP-VSB89IV1>
2024-10-07 12:12:50 +0200

    Initial commit

---
+++ ReservasHotel/src/modelos/Habitacion.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author pc
+ */
+public class Habitacion {
+    public final static int NUMHABITACION = 0;
+    public final static String TIPOHABITACION = " ";
+    public final static int PRECIO = 0;
+    
+    private int numHabitacion;
+    private String tipoHabitacion;
+    private int precio;
+    
+    //Constructor parametrico
+    public Habitacion(int numhabitacion, String tipoHabitacion, int precio){
+        this.numHabitacion=NUMHABITACION;
+        this.tipoHabitacion=TIPOHABITACION;
+        this.precio=PRECIO;
+    }
+    
+    //Constructor vacio
+    public Habitacion(){
+        this(NUMHABITACION, TIPOHABITACION, PRECIO);
+    }
+    
+    //Constructor copia
+    public Habitacion(Habitacion otro){
+        this(otro.numHabitacion, otro.tipoHabitacion, otro.precio);
+    }
+    
+    //Getters y Setters
+    public int getnumHabitacion(){
+        return numHabitacion;
+    }
+    public void setnumHabitacion(){
+        this.numHabitacion=numHabitacion;
+    }
+    
+    public String gettipoHabitacion(){
+        return tipoHabitacion;
+    }
+    public void settipoHabitacion(){
+        this.tipoHabitacion=tipoHabitacion;
+    }
+    
+    public int getprecio(){
+        return precio;
+    }
+    public void setprecio(){
+        this.precio=precio;
+    }
+    
+}
